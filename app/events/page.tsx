37c321ecb70586552f3dd96451ff40b5fc31fa92
Frederick Tan <fredericktan@Fredericks-MacBook-Pro.local>
2025-11-29 21:00:09 +0800

    Fix: Force dynamic rendering for events and insights pages

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,6 +1,8 @@
 import { getEvents } from "@/lib/data";
 import { EventsList } from "@/components/EventsList";
 
+export const dynamic = "force-dynamic";
+
 export default async function EventsPage() {
     const events = await getEvents();
     return <EventsList initialEvents={events} />;
--- app/insights/page.tsx
+++ app/insights/page.tsx
@@ -1,6 +1,8 @@
 import { getReports } from "@/lib/data";
 import { InsightsList } from "@/components/InsightsList";
 
+export const dynamic = "force-dynamic";
+
 export default async function InsightsPage() {
     const reports = await getReports();
     return <InsightsList initialReports={reports} />;
