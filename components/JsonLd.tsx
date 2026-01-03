eb93fdb0cf26aff9571cd6aa4050b745daa70351
FT <fredtan@ftsynergist.com>
2026-01-03 10:46:35 +0800

    Update JsonLd.tsx

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,7 +1,7 @@
 export default function JsonLd() {
     const jsonLd = {
         "@context": "https://schema.org",
-        "@type": "ConsultingService",
+        "@type": "ProfessionalService",
         "name": "FT Synergist",
         "image": "https://www.ftsynergist.com/ft_synergist_logo_wo_background.png",
         "description": "Premium SME Management Consultant & EDG Grant Advisor in Singapore. We help businesses scale through strategic consulting and government grants.",
@@ -18,18 +18,19 @@ export default function JsonLd() {
             "longitude": 103.8465
         },
         "url": "https://www.ftsynergist.com",
-        "telephone": "+6562222222",
+        "telephone": "+6598628906",
         "priceRange": "$$$",
         "areaServed": ["Singapore", "Indonesia", "Vietnam"],
         "founder": {
             "@type": "Person",
             "name": "Frederick Tan"
         },
-        "sameAs": [
-            "https://www.linkedin.com/company/ftsynergist"
-        ]
+"sameAs": [
+      "https://www.linkedin.com/company/ftsynergist",
+      "https://www.linkedin.com/in/tanfrederick/",
+      "https://www.facebook.com/ftsynergist"
+    ]
     };
-
     return (
         <>
             <script
